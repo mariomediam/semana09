e9d946fed1488421bf122ef157b3c5b3c4b14343
mariomediam <54757091+mariomediam@users.noreply.github.com>
2021-10-19 20:53:18 -0500

    Se agrega puerto

--- src/config/server.ts
+++ src/config/server.ts
@@ -12,11 +12,11 @@ export class Server {
   // private => no podra ser accedido desde fuera de la clase
   // readonly => no podra ser modificado su valor afuera del constructor
   private readonly app: Express;
-  private readonly puerto: number;
+  private readonly puerto: unknown;
 
   constructor() {
     this.app = express();
-    this.puerto = 8000;
+    this.puerto = process.env.PORT;
     this.app.use(cors())
     this.bodyParser();
     this.rutas();
--- types.d.ts
+++ types.d.ts
@@ -9,6 +9,7 @@ declare global{
             CLOUDINARY_NAME: string;
             CLOUDINARY_API_KEY: string;
             CLOUDINARY_API_SECRET: string;
+            PORT: number;
         }
     }
 }
